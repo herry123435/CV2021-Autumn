deb3ea88d1257b298caea558a45c369033edb1f0
kskim0119 <dooly9931@snu.ac.kr>
2021-12-05 14:43:15 +0900

    fix warpClothesSegs.py and reset merge with bodySeg

--- warpClothesSegs.py
+++ warpClothesSegs.py
@@ -15,30 +15,35 @@ class BodyParts(Enum):
 
 def warpClothesSegs(segImgs, humanImg, ordered_clothes_points, ordered_pose_points):
     # hyper parameter #
-    Nb = 6 # height of gaussian pyramid
+    Nb = 2 # height of gaussian pyramid
+    divider = pow(2, Nb)
 
-    total_parts_num = 9
+    total_parts_num = 2
     pair_num = [7, 4, 4, 4, 4, 4, 4, 4, 4] # number of pairs for each body part
+    matchStartNum = 0
+
     tried_on = humanImg
-    M2, N2 = tried_on.shape[::2]
+    M2, N2 = tried_on.shape[:2]
+
+    M2_, N2_ = (M2 // divider) * divider, (N2 // divider) * divider
+    tried_on = cv2.resize(tried_on, (N2_, M2_))
+    target_shape = np.array(ordered_pose_points,np.int32).reshape(1,-1,2)
 
     for i in range(total_parts_num):
         # get each clothes segment
         clothes_seg = segImgs[i].astype(np.uint8)
-        M1, N1 = clothes_seg.shape[::2]
+        M1, N1 = clothes_seg.shape[:2]
 
         # resize it to size of pose image
-        clothes = cv2.resize(clothes_seg, (N2, M2)) # direction check required
-        clothes_points = [(int(point[0]*N2/N1), int(point[1]*M2/M1)) for point in ordered_clothes_points]
+        clothes = cv2.resize(clothes_seg, (N2_, M2_)) # direction check required
+        clothes_points = [(int(point[0]*N2_/N1), int(point[1]*M2_/M1)) for point in ordered_clothes_points]
 
         tps = cv2.createThinPlateSplineShapeTransformer()
         
         # prepare target points, source points, and match indices for transformation
-        target_shape = np.array(ordered_pose_points,np.int32).reshape(1,-1,2)
         source_shape = np.array(clothes_points,np.int32).reshape(1,-1,2)
-        N = pair_num[i]
         matches =[]
-        for i in range(0, N):
+        for i in range(matchStartNum, matchStartNum + pair_num[i]):
             matches.append(cv2.DMatch(i,i,0))
 
         # warp clothes with the obtained transformation
@@ -65,30 +70,86 @@ def warpClothesSegs(segImgs, humanImg, ordered_clothes_points, ordered_pose_poin
         
         # laplacian pyramid for warped clothes
         clothes_lp = [clothes_gp[-1]]
-        for i in range(Nb-1, 0, -1):
+        for i in range(Nb, 0, -1):
             wc_u = cv2.pyrUp(clothes_gp[i])
             wc_le = cv2.subtract(clothes_gp[i-1], wc_u)
-            clothes_lp.append(wc_le)
+            clothes_lp.insert(0, wc_le)
         
         # laplacian pyramid for pose
         pose_lp = [pose_gp[-1]]
-        for i in range(Nb-1, 0, -1):
+        for i in range(Nb, 0, -1):
             p_u = cv2.pyrUp(pose_gp[i])
             p_le = cv2.subtract(pose_gp[i-1], p_u)
-            pose_lp.append(p_le)
+            pose_lp.insert(0, p_le)
         
         # combined laplacian pyramid
         combined_lp = []
-        for i in range(Nb, -1, -1):
+        for i in range(Nb+1):
             c_e = np.ndarray(pose_lp[i].shape, dtype=np.uint8)
+            resize_clothes_mask = cv2.resize(clothes_mask, (pose_lp[i].shape[1], pose_lp[i].shape[0]))
+            resize_inv_clothes_mask = cv2.resize(inv_clothes_mask, (pose_lp[i].shape[1], pose_lp[i].shape[0]))
             for j in range(3):
-                c_e[:, :, j] = clothes_mask[:, :] * clothes_lp[i][:, :, j] + inv_clothes_mask[:, :] * pose_lp[i][:, :, j]
+                c_e[:, :, j] = resize_clothes_mask[:, :] * clothes_lp[i][:, :, j] + resize_inv_clothes_mask[:, :] * pose_lp[i][:, :, j]
             combined_lp.append(c_e)
         
         # reconstruction
-        tried_on = combined_lp[0]
-        for i in range(1, Nb):
+        tried_on = combined_lp[-1]
+        for i in range(Nb-1, -1, -1):
             tried_on = cv2.pyrUp(tried_on)
             tried_on = cv2.add(tried_on, combined_lp[i])
 
+        matchStartNum = matchStartNum + pair_num[i]
+
+    tried_on = cv2.resize(tried_on, (N2, M2))
+
     return tried_on
+
+import os
+
+if __name__ == '__main__':
+    segImgs = []
+    clothesSegDir = 'clothesOutput'
+    for i in range(0,9):
+         img = cv2.imread(os.path.join(clothesSegDir, 'seg_{}_clothes.png'.format(i)), cv2.IMREAD_UNCHANGED)
+         segImgs.append(np.copy(img))
+
+    humanDir = 'humanInput'
+    humanImg = cv2.imread(os.path.join(humanDir, 'humanPose01.jpeg'), cv2.IMREAD_COLOR)
+
+    clothes_points = [
+        # BODY
+        (317, 188),
+        (225, 243),
+        (239, 285),
+        (237, 426),
+        (417, 228),
+        (400, 269),
+        (398, 423),
+        # LEFT BRANCHIAL
+        (418, 229),
+        (401, 270),
+        (406, 364),
+        (451, 361),
+    ]
+
+    human_points = [
+        # BODY
+        (564, 333),
+        (286, 479),
+        (381, 605),
+        (368, 929),
+        (826, 493),
+        (745, 607),
+        (723, 929),
+        # LEFT BRANCHIAL
+        (827, 494),
+        (746, 608),
+        (763, 812),
+        (863, 802),
+    ]
+
+    tried_on = warpClothesSegs(segImgs, humanImg, clothes_points, human_points)
+
+    cv2.imshow('tried_on', tried_on)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
